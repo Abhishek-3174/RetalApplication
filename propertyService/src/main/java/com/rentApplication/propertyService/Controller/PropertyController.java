a1b812502d6e37253aca7c69d8a453cc4acd35a5
shashank374014 <121810313027@gitam.in>
2024-03-07 09:12:36 +0530

    hh

--- propertyService/src/main/java/com/rentApplication/propertyService/Controller/PropertyController.java
+++ propertyService/src/main/java/com/rentApplication/propertyService/Controller/PropertyController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.*;
 
 
 @RestController
-@RequestMapping("product")
+@RequestMapping("product") 
 public class PropertyController {
     @Autowired
     PropertyService propertyService;
